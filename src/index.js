4419d68b58f32adacb322c5e4c0b8ad715165c45
Nadezhda Komarova <naduha.9612@gmail.com>
2023-08-09 12:04:54 +0300

    feat: add solution for open-closed brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = ['(', '{', '[', '|'];
+  const bracketsPair = {
+    [')']: '(',
+    ['}']: '{',
+    [']']: '[',
+    ['|']: '|'
+  };
+  
+let stack = [];
+  
+  for (let i = 0; i<str.length; i++) {
+    let currentSymbol = str[i];
+    
+    if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      
+    let topElement = stack[stack.length - 1];
+      
+    if (bracketsPair[currentSymbol] === topElement) {
+      stack.pop();
+    } else {
+      return false;
+    }
+    
+  }
 }
+  return stack.length === 0? true : false
+}
+
